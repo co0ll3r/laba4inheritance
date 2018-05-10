b17495792c32fbd99e2abeffbf11afa1a66aa13a
cooll3r_laptop <co0ll3rhite@gmail.com>
2018-05-10 19:05:26 +0500

    all works, before teeacher's check

---
+++ CompHeader.h
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <set>
+#pragma once
+
+const int N = 100;
+class Perechen;
+class workComputers;
+class aggregate;
+
+struct COMP{
+	std::string ProcName; // Brand name
+	std::string ProcType; // Specification
+	std::string Graphics; // Graphic Card
+	double GraphicVolume;
+	double ClockSpeed;
+	double RAM;
+	double Storage;
+};
+
+struct RECORD{
+	COMP CompInfo;
+	double CompCost;
+	int CompInStock;
+};
+
+struct BrandPerech{
+	std::string ProcName;
+	int Count;
+};
+
+struct TypeProcPerech{
+	std::string ProcType;
+	int Count;
+};
+
+struct VideocardsPerech{
+ 	double GraphicVolume;
+	int Count;
+}; 
+
+class workComputers {
+	public:
+		void InputFromFile();
+		virtual void OutputInFile();
+		void Add_comp();
+		void Delete_comp();
+ 		virtual	void showInfo();
+		virtual void swapElementsInVector(unsigned);
+		virtual	void SortProcTypeAndClock();
+		void SortProcName();
+		void SortPrice();
+
+		friend void makePerechen1(workComputers clWorkComp, Perechen& clPerech);
+		friend void makePerechen2(workComputers clWorkComp, Perechen& clPerech);
+		friend void makePerechen3(workComputers clWorkComp, Perechen& clPerech);
+	protected:
+		std::vector<RECORD> CapabilitiesComp;
+};
+
+class SearchComp : public workComputers{
+	public:
+		void SearchPrice();
+		void SearchHddVolume();
+		void SearchBrandTypeRamETC();
+		void SortRAM();
+		virtual void OutputInFile();
+		virtual void SortProcTypeAndClock();
+		virtual void showInfo();
+		virtual void swapElementsInVector(unsigned);
+	private:
+		std::vector<RECORD> SearchResult;
+};
+
+class Perechen : public SearchComp{
+	public:
+		Perechen() : pBrandlen(0), pProclen(0), pVideolen(0) {}
+		~Perechen() {}
+		Perechen(Perechen&);
+		Perechen operator=(Perechen&);
+		
+		void showFirstPerech();
+		void showSecondPerech();
+		void showThirdPerech();
+		void sortProcTypeFirstPerech();
+		void sortCountSecondPerech();
+		void sortVideoVolumeThirdPerech();
+		void saveFirstPerech();
+		void saveSecondPerech(); 
+		void saveThirdPerech();
+
+		friend void makePerechen1(workComputers clWorkComp, Perechen& clPerech);
+		friend void makePerechen2(workComputers clWorkComp, Perechen& clPerech);
+		friend void makePerechen3(workComputers clWorkComp, Perechen& clPerech);
+		
+	private:
+		int pBrandlen, pProclen, pVideolen;
+		BrandPerech perechenBrands [N];
+		TypeProcPerech perechenProcTypes [N];
+		VideocardsPerech perechenVideocardVolume [N];
+};
